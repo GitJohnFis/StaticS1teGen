cabda6156d2f7164127727a096d2349d3a460001
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-23 01:06:34 +0000

    new txt to html nodes

--- src/textnode.py
+++ src/textnode.py
@@ -24,3 +24,18 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, TextType.{self.text_type.name}, {self.url})"
 
+def text_node_to_html_node(node):
+    if node.text_type == TextType.TEXT:
+        return LeafNode(None, node.text)
+    elif node.text_type == TextType.BOLD:
+        return LeafNode("b", node.text)
+    elif node.text_type == TextType.ITALIC:
+        return LeafNode("i", node.text)
+    elif node.text_type == TextType.CODE:
+        return LeafNode("code", node.text)
+    elif node.text_type == TextType.LINK:
+        return LeafNode("a", node.text, {"href": node.url})
+    elif node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": node.url, "alt": node.text})
+    else:
+        raise ValueError(f"Invalid text type: {node.text_type}")
